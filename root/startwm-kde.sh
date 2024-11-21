a9b9bf61278092cac99d5711523eded332beae8b
lolpc700 <164327657+lolpc700@users.noreply.github.com>
2024-11-21 09:44:02 +0100

    Update startwm-kde.sh

--- root/startwm-kde.sh
+++ root/startwm-kde.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
 setterm blank 0
 setterm powerdown 0
-sudo if ! mountpoint -q /tmp; then
- sudo mount -t tmpfs none /tmp
+
+if ! mountpoint -q /tmp; then
+    sudo mount -t tmpfs none /tmp
 fi
-sudo if ! mountpoint -q /run; then
- sudo mount -t tmpfs none /run
+
+if ! mountpoint -q /run; then
+    sudo mount -t tmpfs none /run
 fi
+
 /usr/bin/dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
