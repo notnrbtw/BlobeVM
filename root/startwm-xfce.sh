503281885f89f56f6eda74c8bfabbfe69c3274c4
lolpc700 <164327657+lolpc700@users.noreply.github.com>
2024-11-21 09:29:56 +0100

    Update startwm-xfce.sh

--- root/startwm-xfce.sh
+++ root/startwm-xfce.sh
@@ -2,5 +2,10 @@
 
 setterm blank 0
 setterm powerdown 0
-
+sudo if ! mountpoint -q /tmp; then
+ sudo mount -t tmpfs none /tmp
+fi
+sudo if ! mountpoint -q /run; then
+ sudo mount -t tmpfs none /run
+fi
 dbus-launch /usr/bin/xfce4-session > /dev/null 2>&1
