045f8c3f1284cf86d84fbebbed983f0c8dba222e
lolpc700 <164327657+lolpc700@users.noreply.github.com>
2024-11-21 09:30:51 +0100

    Update startwm-cinnamon.sh

--- root/startwm-cinnamon.sh
+++ root/startwm-cinnamon.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 setterm blank 0
 setterm powerdown 0
-/usr/bin/cinnamon-session > /dev/null 2>&1
+sudo if ! mountpoint -q /tmp; then
+ sudo mount -t tmpfs none /tmp
+fi
+sudo if ! mountpoint -q /run; then
+ sudo mount -t tmpfs none /run
+fi
+dbus-launch /usr/bin/cinnamon-session > /dev/null 2>&1
