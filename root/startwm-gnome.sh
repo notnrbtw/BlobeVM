64e5d3d41d1bae07c0028518146778a104f866ca
lolpc700 <164327657+lolpc700@users.noreply.github.com>
2024-11-21 09:30:30 +0100

    Update startwm-gnome.sh

--- root/startwm-gnome.sh
+++ root/startwm-gnome.sh
@@ -3,5 +3,11 @@ setterm blank 0
 setterm powerdown 0
 export XDG_CURRENT_DESKTOP=GNOME
 export XDG_SESSION_TYPE=x11
+sudo if ! mountpoint -q /tmp; then
+ sudo mount -t tmpfs none /tmp
+fi
+sudo if ! mountpoint -q /run; then
+ sudo mount -t tmpfs none /run
+fi
 sudo service dbus start
 /usr/bin/gnome-shell --x11 -r > /dev/null 2>&1
