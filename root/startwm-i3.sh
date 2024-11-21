1ac1c2c2d29e4a75c2d6202c2e355a8bc50e5a7c
lolpc700 <164327657+lolpc700@users.noreply.github.com>
2024-11-21 09:30:18 +0100

    Update startwm-i3.sh

--- root/startwm-i3.sh
+++ root/startwm-i3.sh
@@ -2,4 +2,10 @@
 
 setterm blank 0
 setterm powerdown 0
-/usr/bin/i3 > /dev/null 2>&1
+sudo if ! mountpoint -q /tmp; then
+ sudo mount -t tmpfs none /tmp
+fi
+sudo if ! mountpoint -q /run; then
+ sudo mount -t tmpfs none /run
+fi
+dbus-launch /usr/bin/i3 > /dev/null 2>&1
